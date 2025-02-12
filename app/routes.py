2eb98f2b6c0db2cb57f360c323af63d878608bdc
Guilherme Carlos <guicasii5@gamil.com>
2025-02-12 08:39:24 -0300

    Correção de erros na rota curriculo

--- app/routes.py
+++ app/routes.py
@@ -62,4 +62,9 @@ def skills():
 
 @bp.route('/curriculo')
 def baixar_curriculo():
-    return send_file('static/curriculo/curriculo.pdf', as_attachment=True)
+    caminho = os.path.join(os.getcwd(), 'app/static/curriculo/curriculo.pdf')
+    
+    if not os.path.exists(caminho):
+        return "Arquivo não encontrado", 404
+
+    return send_file(caminho, mimetype='application/pdf', as_attachment=False)
